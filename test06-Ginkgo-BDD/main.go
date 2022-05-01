061a8ec607942d2587d3c82bf956229122a58202
Administrator <admin@example.com>
2022-05-01 13:51:47 +0800

    add  test06 ginKgo for BDD development2

--- test06-Ginkgo-BDD/main.go
+++ test06-Ginkgo-BDD/main.go
@@ -1,5 +1,13 @@
 package main
 
+type Person struct {
+	Age int
+}
+
+func (p *Person) IsChild() bool {
+	return p.Age < 18
+}
+
 
 
 
--- test06-Ginkgo-BDD/test06_Ginkgo_BDD_suite_test.go
+++ test06-Ginkgo-BDD/test06_Ginkgo_BDD_suite_test.go
@@ -1,6 +1,7 @@
-package main_test
+package main
 
 import (
+	"log"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -15,3 +16,39 @@ func TestTest06GinkgoBDD(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Test06GinkgoBDD Suite")
 }
+
+var _ = Describe("Person.IsChild", func() {
+	Context("when the person is a child", func() {
+		It("returns true", func() {
+			person := Person{Age: 10}
+			response := person.IsChild()
+			Expect(response).To(BeTrue())
+			//Expect(response).To(BeFalse())
+		})
+	})
+
+	Context("when the person is not a child", func() {
+		BeforeEach(func() {
+			log.Print("not a child")
+		})
+		AfterEach(func() {
+			log.Print("not a child")
+		})
+
+		//XIt means skip the test block
+		//FIt means skip the before test block
+		XIt("returns true", func() {
+			person := Person{Age: 100}
+			response := person.IsChild()
+			Expect(response).To(BeFalse())
+			//Expect(response).To(BeFalse())
+		})
+	})
+
+	
+
+
+
+})
+
+
